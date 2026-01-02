a85217e38b040358eb1354deb91ad32a85c272ad
zeo <goonox35@proton.me>
2026-01-02 16:25:16 +0500

    nothing

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 if not exist "build/" (
     mkdir build
     cd build
-    cmake .. -G"Visual Studio 18 2026" -DCMAKE_BUILD_TYPE=Debug -DRAFX_BUILD_EXAMPLES=ON -DRAFX_D3D12_SUPPORT=ON
+    cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DRAFX_BUILD_EXAMPLES=ON -DRAFX_D3D12_SUPPORT=ON
     cd ..
 )
 
